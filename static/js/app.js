ae5a325497620e5627ec7b5aa967e7d2d45262fb
xiaoyi68 <zhxy@yahoo.com>
2019-09-24 17:34:22 -0500

    hw

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var button = d3.select("#button");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#patient-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+  console.log(filteredData);
+ 
+});

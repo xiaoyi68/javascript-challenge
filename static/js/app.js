fbfa0629bdf589df810453c6a53e8803dda9c1d1
xiaoyi68 <zhxy@yahoo.com>
2019-09-30 10:27:32 -0500

    hw

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,39 @@ button.on("click", function() {
   console.log(filteredData);
  
 });
+
+
+var tbody=d3.select("tbody");
+
+// appends a table
+
+function result(filterData){
+    tbody.html("")
+    filterData.forEach(report => {
+    var row=tbody.append("tr");
+    Object.entries(report).forEach(([key,value])=>{
+        var cell=row.append("td");
+        cell.text(value);
+    });
+});
+}
+
+result(tableData)
+
+// select submit button
+var submit=d3.select("#filter-btn")
+
+submit.on("click",function(){
+    d3.event.preventDefault();
+    var inputDateV=inputDate.property("value");
+    var inputCityV=inputCity.property("value").trim().toLowerCase();
+    var inputStateV=inputState.property("value").trim().toLowerCase();
+    var inputCountryV=inputCountry.property("value").trim().toLowerCase();
+    var inputShapeV=inputShape.property("value").trim().toLowerCase();
+    //console.log(inputDateV)
+
+    var response=tableData.filter(data=>data.datetime===inputDateV);
+
+    result(response)
+})
+

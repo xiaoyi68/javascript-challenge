fb9b58c895a4436e05bfe68d1856b5f792cb8018
xiaoyi68 <zhxy@yahoo.com>
2019-10-09 15:52:56 -0500

    hw

--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,7 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-var button = d3.select("#filter-btn");
-
-button.on("click", function() {
-
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  console.log(inputValue);
-  console.log(tableData);
-
-  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-
-  console.log(filteredData);
- 
-});
-
-
+// load table
 var tbody=d3.select("tbody");
 
 // appends a table
@@ -39,20 +19,96 @@ function result(filterData){
 
 result(tableData)
 
-// select submit button
-var submit=d3.select("#filter-btn")
+// =========================  First basic search part================== //
 
-submit.on("click",function(){
+// set button variables
+var first_filter=d3.select("#f-filter-btn");
+var first_reset=d3.select("#f-reset-btn");
+
+// set input box variables
+var first_input=d3.select("#datetime");
+
+
+// filter function
+first_filter.on("click",function(){
     d3.event.preventDefault();
-    var inputDateV=inputDate.property("value");
-    var inputCityV=inputCity.property("value").trim().toLowerCase();
-    var inputStateV=inputState.property("value").trim().toLowerCase();
-    var inputCountryV=inputCountry.property("value").trim().toLowerCase();
-    var inputShapeV=inputShape.property("value").trim().toLowerCase();
-    //console.log(inputDateV)
+    var first_inputV=first_input.property("value");
+    console.log(first_inputV)
 
-    var response=tableData.filter(data=>data.datetime===inputDateV);
-
+    var response=tableData.filter(data=>data.datetime===first_inputV);
     result(response)
 })
 
+
+// reset table and input function
+first_reset.on("click",function(){
+    tbody.html("")
+    result(tableData)
+    first_input.property("value","")
+})
+
+
+// =========================  second advance search part================== //
+var value=d3.select("#value");
+var criter=d3.select("#criteria")
+var add=d3.select("#add")
+var addB=d3.select("#add-btn")
+var reset=d3.select("#reset-btn");
+var secFilter=d3.select("#filter-btn");
+var resetC=d3.select("#reset-btn")
+
+
+var filter={}
+
+addB.on("click",function(){
+    var items=criter.property("value")
+    var v=value.property("value").trim();
+     if (items==="Date"){
+        items=items.trim()
+    }
+    else{
+        v=v.toLowerCase()
+    }
+    add.append("li")
+     .text(items+":"+v)
+
+     filter[items]=v
+
+    criter.property("value","")
+    value.property("value","")
+})
+
+
+
+secFilter.on("click",function(){
+
+    lens=Object.keys(filter).length
+
+
+    if (lens==1){
+        input=Object.values(filter)[0]
+        key=Object.keys(filter)[0]
+        if (key=="Date"){
+            k="datetime"
+        }
+        else {
+            k=key.toLowerCase()
+        }
+
+        var response=tableData.filter(data=>data[k]==input)
+           result(response)
+}
+
+     }
+
+)
+
+resetC.on("click",function(){
+    tbody.html("")
+    criter.property("value","")
+    value.property("value","")
+    add.html("")
+    result(tableData)
+    filter={}
+    add.selectAll("li").remove()
+})
